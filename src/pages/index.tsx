cb2f78fae54cf8b127dc3640c5b764d1aec589c8
ykhost <andrew.heringer1337@gmail.com>
2022-05-17 21:59:39 -0300

    refactor: :art: add responsive in section "call"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,10 @@ export default function Home() {
 			<Banner />
 			<TravelTypes />
 			<Flex justifyContent="center" w="100vw">
-				<Box w={"5.625rem"} border="1px" m={"5rem"} borderColor="gray.700" />
+				<Box w={["3.75rem", "5.625rem"]} border="1px" m={["1.25rem", "5rem"]} borderColor="gray.700" />
 			</Flex>
 			<Box display="block">
-				<Text textAlign="center" fontSize={"2.25rem"} fontWeight="medium">Vamos nessa? <br/>Então escolha seu continente</Text>
+				<Text textAlign="center" fontSize={["1.25rem", "2.25rem"]} fontWeight="medium">Vamos nessa? <br/>Então escolha seu continente</Text>
 			</Box>
 			<Slide />
 		</Flex>
